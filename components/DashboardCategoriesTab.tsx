2c90e19417e362b31d7d6001ea9bdc8e17aa1f2b
oda404 <xdxalexandru404@gmail.com>
2022-12-26 16:32:41 +0200

    Dashboard: Remove a box shadow.

--- components/DashboardCategoriesTab.tsx
+++ components/DashboardCategoriesTab.tsx
@@ -503,7 +503,7 @@ function MobileViewDashboardButtons({ default_currency }: MobileViewDashboardBut
                     flexDirection="column"
                     padding={"6px"}
                     border={"none"}
-                    sx={{ borderRadius: "5px", boxShadow: "rgba(0, 0, 0, 0.25) 0px 54px 55px, rgba(0, 0, 0, 0.12) 0px -12px 30px, rgba(0, 0, 0, 0.12) 0px 4px 6px, rgba(0, 0, 0, 0.17) 0px 12px 13px, rgba(0, 0, 0, 0.09) 0px -3px 5px" }}
+                    sx={{ borderRadius: "5px" }}
                 >
                     <Box display="flex">
                         <Box sx={{ fontSize: "14px", color: "var(--exxpenses-light-green)" }} marginLeft="6px">
